9ae9c34b7b5e208ebd0375656337f853f1c14f70
Ely Alamillo <ely0013@gmail.com>
2018-06-16 13:37:16 -0500

    inital setup

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Ely Alamillo',
+    siteUrl: 'https://www.elyalamillo.com',
+    description: 'My creative outlet',
+  },
+  pathPrefix: '',
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+}
