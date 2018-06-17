ec3090f869b1e73553108af7b4908c014fdbdba8
Ely Alamillo <ely0013@gmail.com>
2018-06-16 21:41:08 -0500

    added aboutMe component

---
+++ src/components/aboutMe.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const AboutMe = () => (
+  <section className="section is-medium has-text-centered has-background-info">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-three-fifths">
+          <h1 className="title is-spaced is-size-desktop is-size-4-mobile has-text-white">
+            Hi, I'm Ely. Nice to mean you.
+          </h1>
+          <h2 className="subtitle is-size-5-desktop has-text-white">
+            Since beginning my journey as a freelance designer nearly 8 years
+            ago, I've done remote work for agencies, consulted for startups, and
+            collaborated with talented people to create digital products for
+            both business and consumer use. I'm quietly confident, naturally
+            curious, and perpetually improving my chops.
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default AboutMe
