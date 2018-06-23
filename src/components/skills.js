56baf4acb1c02c5bc3e48937019e6729d6be2997
Ely Alamillo <ely0013@gmail.com>
2018-06-22 19:14:08 -0500

    added skill section

---
+++ src/components/skills.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './skills.scss'
+
+const Skills = () => (
+  <section className="section has-text-centered has-background-success">
+    <div className="container">
+      <div className="box">
+        <div className="content">
+          <div className="columns has-text-center">
+            <div className="column skills-section">
+              <h1>Backend</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                sed dignissimos corporis reiciendis architecto voluptas at nemo
+                sunt! Et eaque impedit incidunt animi, quam nostrum rerum
+                repellat assumenda voluptate quis.
+              </p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend tech I use:
+              </p>
+              <p>Node, Express, MySql</p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend Tools:
+              </p>
+              <p>Vscode, Mlab, JS Debugger</p>
+            </div>
+            <div className="column skills-section">
+              <h1>Backend</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                sed dignissimos corporis reiciendis architecto voluptas at nemo
+                sunt! Et eaque impedit incidunt animi, quam nostrum rerum
+                repellat assumenda voluptate quis.
+              </p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend tech I use:
+              </p>
+              <p>Node, Express, MySql</p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend Tools:
+              </p>
+              <p>Vscode, Mlab, JS Debugger</p>
+            </div>
+            <div className="column">
+              <h1>Backend</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                sed dignissimos corporis reiciendis architecto voluptas at nemo
+                sunt! Et eaque impedit incidunt animi, quam nostrum rerum
+                repellat assumenda voluptate quis.
+              </p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend tech I use:
+              </p>
+              <p>Node, Express, MySql</p>
+              <p className="has-text-weight-semibold has-text-link">
+                Backend Tools:
+              </p>
+              <p>Vscode, Mlab, JS Debugger</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Skills
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Link from 'gatsby-link'
 
 import Hero from '../components/hero'
 import AboutMe from '../components/aboutMe'
+import Skills from '../components/skills'
 
 const IndexPage = () => (
   <div>
     <Hero />
     <AboutMe />
+    <Skills />
   </div>
 )
 
