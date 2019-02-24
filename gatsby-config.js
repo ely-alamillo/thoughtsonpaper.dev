aa4827bac32b67327596f3bd8e9aa61c7eb0c5d1
Ely Alamillo <ely0013@gmail.com>
2019-02-24 00:24:00 -0600

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,22 @@ module.exports = {
     description: 'My creative outlet',
   },
   pathPrefix: '/',
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-135086350-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        respectDNT: false,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'elyalamillo.com',
+        cookieName: 'x-tracking',
+      },
+    },
+  ],
 }
