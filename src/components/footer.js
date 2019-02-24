c880a96fd55a6e9a7e67d35641eb6f0760049ada
Ely Alamillo <ely0013@gmail.com>
2019-02-23 21:33:03 -0600

    fixc social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,17 +7,23 @@ const Footer = () => (
     <div className="container">
       <div className="has-text-centered">
         <div className="nav-center">
-          <a className="nav-item">
+          <a className="nav-item" href="https://github.com/ely-alamillo">
             <span className="icon is-large">
               <i className="fab fa-github fa-2x" />
             </span>
           </a>
-          <a className="nav-item">
+          <a
+            className="nav-item"
+            href="https://www.linkedin.com/in/elyalamillo/"
+          >
             <span className="icon is-large">
               <i className="fab fa-linkedin fa-2x" />
             </span>
           </a>
-          <a className="nav-item">
+          <a
+            className="nav-item"
+            href="mailto:ely0013@gmail.com?Subject=Hello%20Ely"
+          >
             <span className="icon is-large">
               <i className="fas fa-envelope fa-2x" />
             </span>
