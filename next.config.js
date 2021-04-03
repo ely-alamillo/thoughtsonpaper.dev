49a3c52f2b2816a0b8034825dd9399c13e164e37
Ely Alamillo <ely0013@gmail.com>
2021-04-02 19:57:32 -0500

    add md loader

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+};
