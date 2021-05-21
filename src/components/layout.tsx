b5335823b5af6422a4de361f13ea48fb54d4b56a
Ely Alamillo <ely0013@gmail.com>
2021-05-21 14:57:24 -0500

    feat(next-deploy): remove favicon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -84,7 +84,7 @@ export const Layout: FunctionComponent<LayoutProps> = ({
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <button
             type="button"
-            className="theme-switch-button"
+            className="theme-switch-button focus:shadow-none focus:outline-none"
             onClick={() => switchTheme()}
           >
             {theme === 'dark' ? (
